e1dac21cdc6821f6291cdffa9d5d9b2deaba6c8f
Zoe <zoethomas1540@gmail.com>
2021-12-29 21:37:37 +0000

    Configured JWT authentication in Spring

---
+++ src/main/java/com/vegetables/website/model/AuthenticationResponse.java
@@ -0,0 +1,14 @@
+package com.vegetables.website.model;
+
+public class AuthenticationResponse {
+
+    private final String jwt;
+
+    public AuthenticationResponse(String jwt) {
+        this.jwt = jwt;
+    }
+
+    public String getJwt() {
+        return jwt;
+    }
+}
---
+++ src/test/java/com/vegetables/website/WebsiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vegetables.website;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebsiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
